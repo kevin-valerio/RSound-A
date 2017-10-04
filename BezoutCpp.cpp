47776bf406d2d43e85ab9002b559ba6a6c3831eb
Kevin Valerio <kevin.valerio@etu.univ-amu.fr>
2017-10-04 15:38:20 +0200

    Trying to find Bezout coefficients

--- BezoutCpp.cpp
+++ BezoutCpp.cpp
@@ -4,12 +4,9 @@ using namespace std;
 
 void afficheMatrice(vector<vector<int>> MatriceOperation){
 
-    for(int x=0;x<MatriceOperation.size();x++)
-     {
+    for(int x=0;x<MatriceOperation.size();x++) {
          for(int y=0;y<MatriceOperation[x].size();y++)
-         {
              cout << MatriceOperation[x][y] << "    ";
-         }
          cout << endl;
      }
 }
@@ -40,16 +37,48 @@ void Bezout(float X, float Y){
 
      }
 
-   // afficheMatrice(MatriceOperation);
-
     int nombreEtape = MatriceOperation.size();
+    int A,B,U,V;
 
-    for(unsigned i(nombreEtape); i > 0; --i){
-        a = MatriceOperation[i-1][0];
-        q = MatriceOperation[i-1][1];
-        b = MatriceOperation[i-1][2];
-        r = MatriceOperation[i-1][3];
+    for(unsigned i(0); i < nombreEtape -1 ; ++i){
+
+        a = MatriceOperation[i][0];
+        q = MatriceOperation[i][1];
+        b = MatriceOperation[i][2];
+        r = MatriceOperation[i][3];
+        // cout << a << " = " << q << "×" << b << " + " << r << endl;
     }
+
+     /*
+             a[i]     q[i]    b[i]    r[i]
+             1000   =  3  ×   257  +  229
+             257   =   1  ×   229  +   28
+             229   =   8  ×    28  +    5
+             28   =    5  ×     5  +    3
+             5   =    q 1  ×     3  +    2
+             3   =     1  ×     2  +    1
+
+    */
+
+     U = 1;
+     B = MatriceOperation[nombreEtape -1][2];
+     A = MatriceOperation[nombreEtape -1][0];
+     V = -(MatriceOperation[nombreEtape -1][1]) ;
+
+     int cpt = 0;
+
+     while(A != X && B != Y){
+        int tmp = U;
+
+        ++cpt;
+        A = MatriceOperation[cpt-1][0];
+        U = V;
+        B = A;
+        V = tmp - (MatriceOperation[cpt-1][1]) * V;
+
+
+     }
+     cout << U << " " << B << " " << V << " " << A;
 }
 
 int main()
